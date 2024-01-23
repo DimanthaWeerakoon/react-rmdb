30bca221cf342a70c09b5968dfae48237f3cf5de
Dimantha Thiranjaya Weerakoon <dweerakoon71@gmail.com>
2024-01-23 23:43:40 +0530

    custom hook created to fetch movies

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,40 +8,13 @@ import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
 
 //Compnents
 
-//Hook
-
+//Hooks
+import { useHomeFetch } from "../hooks/useHomeFetch";
 //Image
 import NoImage from "../images/no_image.jpg";
 
 const Home = () => {
-  const [state, setState] = useState();
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
-
-  const fetchMovies = async (page, searchTerm = "") => {
-    try {
-      setError(false);
-      setLoading(true);
-
-      const movies = await API.fetchMovies(searchTerm, page);
-      console.log(movies);
-
-      setState((prev) => ({
-        ...movies,
-        results:
-          page > 1 ? [...prev.results, ...movies.length] : [...movies.results],
-      }));
-    } catch (error) {
-      setError(true);
-    }
-
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    fetchMovies(1);
-  }, []);
-
+  const { state, loading, error } = useHomeFetch();
   console.log(state);
 
   return <div>Home Page</div>;
---
+++ src/hooks/useHomeFetch.js
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react";
+//API
+import API from "../API";
+
+const initialState = {
+  page: 1,
+  results: [],
+  total_pages: 0,
+  total_results: 0,
+};
+
+export const useHomeFetch = () => {
+  const [state, setState] = useState();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const fetchMovies = async (page, searchTerm = "") => {
+    try {
+      setError(false);
+      setLoading(true);
+
+      const movies = await API.fetchMovies(searchTerm, page);
+      console.log(movies);
+
+      setState((prev) => ({
+        ...movies,
+        results:
+          page > 1 ? [...prev.results, ...movies.length] : [...movies.results],
+      }));
+    } catch (error) {
+      setError(true);
+    }
+
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchMovies(1);
+  }, []);
+
+  return { state, loading, error };
+};
