cb420068f509385cefeb033eaf35330eef24df5e
Dimantha Thiranjaya Weerakoon <dweerakoon71@gmail.com>
2024-01-24 00:24:55 +0530

    Hero image added on home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,6 +7,7 @@ import API from "../API";
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
 
 //Compnents
+import HeroImage from "./HeroImage";
 
 //Hooks
 import { useHomeFetch } from "../hooks/useHomeFetch";
@@ -17,7 +18,17 @@ const Home = () => {
   const { state, loading, error } = useHomeFetch();
   console.log(state);
 
-  return <div>Home Page</div>;
+  return (
+    <>
+      {state.results[0] ? (
+        <HeroImage
+          image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+          title={state.results[0].original_title}
+          text={state.results[0].overview}
+        />
+      ) : null}
+    </>
+  );
 };
 
 export default Home;
