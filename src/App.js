c9540e89a8a6ecb7bd40a240f8681a70a84c78ac
Dimantha Thiranjaya Weerakoon <dweerakoon71@gmail.com>
2024-01-23 16:31:08 +0530

    Header created with logos and styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
-
+// Components
+import Header from "./components/Header";
 // Styles
 import { GlobalStyle } from "./GlobalStyle";
 
 function App() {
   return (
     <div className="App">
-      Start here.
+      <Header />
+      Start here
       <GlobalStyle />
     </div>
   );
