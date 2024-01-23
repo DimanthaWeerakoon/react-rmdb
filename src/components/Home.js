3f562c6886d29a629f37c38f8d26be0eac7f3468
Dimantha Thiranjaya Weerakoon <dweerakoon71@gmail.com>
2024-01-23 23:32:18 +0530

    fetched movie data from the API

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,7 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+
+//API
+import API from "../API";
 
 //Config
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
@@ -14,6 +17,32 @@ const Home = () => {
   const [state, setState] = useState();
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
+
+  const fetchMovies = async (page, searchTerm = "") => {
+    try {
+      setError(false);
+      setLoading(true);
+
+      const movies = await API.fetchMovies(searchTerm, page);
+      console.log(movies);
+
+      setState((prev) => ({
+        ...movies,
+        results:
+          page > 1 ? [...prev.results, ...movies.length] : [...movies.results],
+      }));
+    } catch (error) {
+      setError(true);
+    }
+
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchMovies(1);
+  }, []);
+
+  console.log(state);
 
   return <div>Home Page</div>;
 };
