d899aed75c352fbf218be0c2b0b3bead85f4343d
Dimantha Thiranjaya Weerakoon <dweerakoon71@gmail.com>
2024-01-25 05:14:00 +0530

    Controller added to searchbar

--- src/components/Home.js
+++ src/components/Home.js
@@ -19,7 +19,7 @@ import { useHomeFetch } from "../hooks/useHomeFetch";
 import NoImage from "../images/no_image.jpg";
 
 const Home = () => {
-  const { state, loading, error } = useHomeFetch();
+  const { state, loading, error, setSearchTerm } = useHomeFetch();
   console.log(state);
 
   return (
@@ -31,7 +31,7 @@ const Home = () => {
           text={state.results[0].overview}
         />
       ) : null}
-      <SearchBar />
+      <SearchBar setSearchTerm={setSearchTerm} />
       <Grid header="Popular movies">
         {state.results.map((movie) => (
           <Thumb
--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -5,12 +5,27 @@ import searchIcon from "../../images/search-icon.svg";
 //Styles
 import { Wrapper, Content } from "./SearchBar.styles";
 
-const SearchBar = ({ setSarchTerm }) => {
+const SearchBar = ({ setSearchTerm }) => {
+  const [state, setState] = useState("");
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setSearchTerm(state);
+    }, 500);
+
+    return () => clearTimeout(timer);
+  }, [setSearchTerm, state]);
+
   return (
     <Wrapper>
       <Content>
         <img src={searchIcon} alt="search icon" />
-        <input type="text" placeholder="Search" />
+        <input
+          type="text"
+          placeholder="Search"
+          onChange={(event) => setState(event.currentTarget.value)}
+          value={state}
+        />
       </Content>
     </Wrapper>
   );
