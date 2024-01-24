b4edeca28a8a7e61c4309b3338f6031e936ce221
Dimantha Thiranjaya Weerakoon <dweerakoon71@gmail.com>
2024-01-25 01:34:07 +0530

    HeroImage styles added with description styles

--- src/hooks/useHomeFetch.js
+++ src/hooks/useHomeFetch.js
@@ -3,14 +3,14 @@ import { useState, useEffect } from "react";
 import API from "../API";
 
 const initialState = {
-  page: 1,
+  page: 0,
   results: [],
   total_pages: 0,
   total_results: 0,
 };
 
 export const useHomeFetch = () => {
-  const [state, setState] = useState();
+  const [state, setState] = useState(initialState);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
 
