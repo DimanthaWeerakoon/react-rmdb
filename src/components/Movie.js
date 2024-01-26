86c32cbd347cfbd8673e345eaaa2c032bc2daabc
Dimantha Thiranjaya Weerakoon <dweerakoon71@gmail.com>
2024-01-26 15:22:43 +0530

    Movie BreadCrumb added

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -6,6 +6,7 @@ import { useParams } from "react-router-dom";
 import { IMAGE_BASE_URL, POSTER_SIZE } from "../config";
 
 //Components
+import BreadCrumb from "./BreadCrumb";
 import Grid from "./Grid";
 import Spinner from "./Spinner";
 
@@ -20,10 +21,11 @@ const Movie = () => {
 
   const { state: movie, loading, error } = useMovieFetch(movieId);
 
-  console.log(movie);
+  if (loading) return <Spinner />;
+  if (error) return <div>Something went wrong...</div>;
   return (
     <>
-      <div>Movie</div>
+      <BreadCrumb moiveTitle={movie.title} />
     </>
   );
 };
