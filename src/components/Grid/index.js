8e1d2188f8015e15a33f709f6aac835d14bf1557
Dimantha Thiranjaya Weerakoon <dweerakoon71@gmail.com>
2024-01-29 18:58:01 +0530

    Grid PropTypes added

--- src/components/Grid/index.js
+++ src/components/Grid/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 //Styles
 
@@ -10,5 +11,9 @@ const Grid = ({ header, children }) => (
     <Content>{children}</Content>
   </Wrapper>
 );
+
+Grid.propTypes = {
+  header: PropTypes.string,
+};
 
 export default Grid;
