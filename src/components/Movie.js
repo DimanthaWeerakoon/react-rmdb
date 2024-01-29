cf42e1196a84ef95a90e4b0625e0be8a92c4deb2
Dimantha Thiranjaya Weerakoon <dweerakoon71@gmail.com>
2024-01-29 14:33:47 +0530

    Actor Component Created

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -11,6 +11,7 @@ import Grid from "./Grid";
 import Spinner from "./Spinner";
 import MovieInfo from "./MovieInfo";
 import MovieInfoBar from "./MovieInfoBar";
+import Actor from "./Actor";
 
 // Hook
 import { useMovieFetch } from "../hooks/useMovieFetch";
@@ -34,6 +35,20 @@ const Movie = () => {
         budget={movie.budget}
         revenue={movie.revenue}
       />
+      <Grid header="Actors">
+        {movie.actors.map((actor) => (
+          <Actor
+            key={actor.credit_id}
+            name={actor.name}
+            character={actor.character}
+            imageUrl={
+              actor.profile_path
+                ? `${IMAGE_BASE_URL}${POSTER_SIZE}${actor.profile_path}`
+                : NoImage
+            }
+          />
+        ))}
+      </Grid>
     </>
   );
 };
